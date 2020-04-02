ccdf73f24e31ca06cac49916668413e676bf9524
Nathan Getachew <nathygech111@gmail.com>
2020-04-03 00:51:14 +0300

    :art: Make Analysis UI fullscreen

--- app/src/main/java/hanalyst/application/hanalystclub/Analysis.java
+++ app/src/main/java/hanalyst/application/hanalystclub/Analysis.java
@@ -2,6 +2,7 @@ package hanalyst.application.hanalystclub;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class Analysis extends Activity {
 
@@ -9,6 +10,17 @@ public class Analysis extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
 }
