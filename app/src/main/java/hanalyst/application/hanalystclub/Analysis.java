9b1b4e4c2e4c9a4617a13ad2055b95122fab1fdd
Nathan Getachew <nathygech111@gmail.com>
2020-04-03 00:32:58 +0300

    (Analysis): Create new activity

---
+++ app/src/main/java/hanalyst/application/hanalystclub/Analysis.java
@@ -0,0 +1,14 @@
+package hanalyst.application.hanalystclub;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Analysis extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis);
+    }
+
+}
