ea0fdccaea5b073a1a3197dcd3c02f495cb52023
Nathan Getachew <nathygech111@gmail.com>
2020-04-03 00:39:29 +0300

    :construction: WIP activity transition

--- app/src/main/java/hanalyst/application/hanalystclub/Login.java
+++ app/src/main/java/hanalyst/application/hanalystclub/Login.java
@@ -1,13 +1,24 @@
 package hanalyst.application.hanalystclub;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends Activity {
-
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.login_button);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Analysis.class));
+            }
+        });
     }
 }
