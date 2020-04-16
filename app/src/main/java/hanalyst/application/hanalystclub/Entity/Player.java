e31179bfd5ece68ac27d7e76269716abdf83d19d
Nathan Getachew <nathygech111@gmail.com>
2020-04-16 11:04:23 +0300

    :sparkles: Add new entity for player

---
+++ app/src/main/java/hanalyst/application/hanalystclub/Entity/Player.java
@@ -0,0 +1,54 @@
+package hanalyst.application.hanalystclub.Entity;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class Player {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private int tNumber;
+    private String name;
+    private String teamId;
+    private String history;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTNumber() {
+        return tNumber;
+    }
+
+    public void setTNumber(int tNumber) {
+        this.tNumber = tNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    public void setHistory(String history) {
+        this.history = history;
+    }
+}
