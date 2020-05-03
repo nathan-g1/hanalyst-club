16da44f06e49fa0a3b8fd6460d7f1b84312937ab
Nathan Getachew <nathygech111@gmail.com>
2020-05-03 23:22:57 +0300

    :fire: Update db version and add notation dao on db

--- app/src/main/java/hanalyst/application/hanalystclub/Database/HAnalystDb.java
+++ app/src/main/java/hanalyst/application/hanalystclub/Database/HAnalystDb.java
@@ -2,14 +2,17 @@ package hanalyst.application.hanalystclub.Database;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import hanalyst.application.hanalystclub.Entity.Notation;
 import hanalyst.application.hanalystclub.Entity.Player;
 import hanalyst.application.hanalystclub.Entity.Team;
+import hanalyst.application.hanalystclub.dao.NotationDao;
 import hanalyst.application.hanalystclub.dao.PlayerDao;
 import hanalyst.application.hanalystclub.dao.TeamDao;
 
-@Database(entities = {Player.class, Team.class}, version = 1, exportSchema = false)
+@Database(entities = {Player.class, Team.class, Notation.class}, version = 2, exportSchema = false)
 public abstract class HAnalystDb extends RoomDatabase {
     public abstract PlayerDao daoAccess();
     public abstract TeamDao teamDao();
+    public abstract NotationDao notationDao();
 }
 
