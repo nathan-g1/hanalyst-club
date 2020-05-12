2161d94aa1416ecb32184ba2dbdbe5ec8b743925
Nathan Getachew <nathygech111@gmail.com>
2020-05-12 16:10:32 +0300

    :sparkles: Add shared preference class

---
+++ app/src/main/java/hanalyst/application/hanalystclub/Util/SharedPreferenceHAn.java
@@ -0,0 +1,20 @@
+package hanalyst.application.hanalystclub.Util;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+public class SharedPreferenceHAn {
+    Context context;
+    private static final String TEAM_ID  = "TEAM_ID";
+    public SharedPreferenceHAn(Context context){
+        this.context = context;
+    }
+
+    public void setTeamId(String teamId) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(TEAM_ID, teamId).apply();
+    }
+
+    public String getTeamId() {
+        return PreferenceManager.getDefaultSharedPreferences(context).getString(TEAM_ID, "null");
+    }
+}
