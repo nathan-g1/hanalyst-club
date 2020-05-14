06ea9463cbcdf6db545cc7e3039357cee6161a6b
Nathan Getachew <nathygech111@gmail.com>
2020-05-14 15:15:28 +0300

    Rename reference

--- app/src/main/java/hanalyst/application/hanalystclub/Database/HAnalystDb.java
+++ app/src/main/java/hanalyst/application/hanalystclub/Database/HAnalystDb.java
@@ -14,7 +14,7 @@ import hanalyst.application.hanalystclub.dao.TeamDao;
 
 @Database(entities = {Player.class, Team.class, Notation.class}, version = 2, exportSchema = false)
 public abstract class HAnalystDb extends RoomDatabase {
-    public abstract PlayerDao daoAccess();
+    public abstract PlayerDao playerDao();
     public abstract TeamDao teamDao();
     public abstract NotationDao notationDao();
 
