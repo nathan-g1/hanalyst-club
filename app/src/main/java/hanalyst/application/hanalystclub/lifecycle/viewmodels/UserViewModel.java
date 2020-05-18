df89a9715f36f83840133a606b3232ab368bd518
Nathan Getachew <nathygech111@gmail.com>
2020-05-19 00:42:35 +0300

    Create a ViewModel for User

---
+++ app/src/main/java/hanalyst/application/hanalystclub/lifecycle/viewmodels/UserViewModel.java
@@ -0,0 +1,36 @@
+package hanalyst.application.hanalystclub.lifecycle.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import hanalyst.application.hanalystclub.Entity.User;
+import hanalyst.application.hanalystclub.repository.UserRepository;
+
+public class UserViewModel extends AndroidViewModel {
+    private UserRepository userRepository;
+    private LiveData<User> userLiveData;
+
+    public UserViewModel(@NonNull Application application) {
+        super(application);
+        userRepository = new UserRepository(application);
+        userLiveData = userRepository.getUser();
+    }
+
+    public void insertUser(User user) {
+        userRepository.insertUser(user);
+    }
+
+    public void updateUser(User user) {
+        userRepository.updateUser(user);
+    }
+
+    public User getLoggedInUser() {
+        return userLiveData.getValue();
+    }
+
+    public void deleteUser(User user) {
+        userRepository.deleteUser(user);
+    }
+}
