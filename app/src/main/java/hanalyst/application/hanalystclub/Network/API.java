cfaa566cb49043f00ed139ac187b2ba7660d967e
Nathan Getachew <nathygech111@gmail.com>
2020-05-30 10:33:50 +0300

    Update api get list of gameTypes from remote

--- app/src/main/java/hanalyst/application/hanalystclub/Network/API.java
+++ app/src/main/java/hanalyst/application/hanalystclub/Network/API.java
@@ -1,6 +1,9 @@
 package hanalyst.application.hanalystclub.Network;
 
+import java.util.List;
+
 import hanalyst.application.hanalystclub.Entity.remote.ClubUser;
+import hanalyst.application.hanalystclub.Entity.remote.RGameType;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -9,4 +12,7 @@ public interface API {
 
     @GET("clubusers/findOne?")
     Call<ClubUser> getUser(@Query("filter") String param);
+
+    @GET("GameTypes")
+    Call<List<RGameType>> getGameTypes();
 }
