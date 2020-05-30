b4a642c22b3d7c5df4749f2d26d335f72fea2db4
Nathan Getachew <nathygech111@gmail.com>
2020-05-30 12:09:01 +0300

    Add player to a team

--- app/src/main/java/hanalyst/application/hanalystclub/Network/API.java
+++ app/src/main/java/hanalyst/application/hanalystclub/Network/API.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import hanalyst.application.hanalystclub.Entity.remote.ClubUser;
 import hanalyst.application.hanalystclub.Entity.remote.RGameType;
+import hanalyst.application.hanalystclub.Entity.remote.RPlayer;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface API {
@@ -15,4 +20,16 @@ public interface API {
 
     @GET("GameTypes")
     Call<List<RGameType>> getGameTypes();
+
+    @GET("players?")
+    Call<List<RPlayer>> getPlayerInATeam(@Query("filter") String param);
+
+    @FormUrlEncoded
+    @POST("players")
+    Call<RPlayer> addAPlayerToATeam(
+            @Field("name") String fullName,
+            @Field("tnumber") int tNumber,
+            @Field("teamId") String teamId
+    );
+
 }
