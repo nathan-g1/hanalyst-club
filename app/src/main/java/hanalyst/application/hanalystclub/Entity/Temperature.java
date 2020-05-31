1d242272f7ade0f31fb8ecd3326ae9e8962790e5
Nathan Getachew <nathygech111@gmail.com>
2020-06-01 00:39:27 +0300

    :sparkles: Update Temperature model to implement serializable

--- app/src/main/java/hanalyst/application/hanalystclub/Entity/Temperature.java
+++ app/src/main/java/hanalyst/application/hanalystclub/Entity/Temperature.java
@@ -1,9 +1,22 @@
 package hanalyst.application.hanalystclub.Entity;
 
-import androidx.room.Entity;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
-@Entity
-public class Temperature {
+import java.io.Serializable;
+
+
+public class Temperature implements Serializable {
+
+    @SerializedName("scaleType")
+    @Expose
     private String scaleType;
+    @SerializedName("value")
+    @Expose
     private double value;
+
+    public Temperature(String scaleType, double value) {
+        this.scaleType = scaleType;
+        this.value = value;
+    }
 }
