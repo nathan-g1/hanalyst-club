0815949c4cbf25c02a638cbf9265942f688f33fb
Nathan Getachew <nathygech111@gmail.com>
2020-06-03 02:54:45 +0300

    :sparkles: Display list of players on WHAT clicked

--- app/src/main/java/hanalyst/application/hanalystclub/lifecycle/Analysis.java
+++ app/src/main/java/hanalyst/application/hanalystclub/lifecycle/Analysis.java
@@ -1,13 +1,16 @@
 package hanalyst.application.hanalystclub.lifecycle;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,19 +20,25 @@ import java.util.TimerTask;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import hanalyst.application.hanalystclub.Adapter.AttackAdapter;
 import hanalyst.application.hanalystclub.Adapter.DefenseAdapter;
+import hanalyst.application.hanalystclub.Adapter.PlayersAdapter;
 import hanalyst.application.hanalystclub.Entity.Attack;
 import hanalyst.application.hanalystclub.Entity.Defense;
 import hanalyst.application.hanalystclub.Entity.Player;
 import hanalyst.application.hanalystclub.R;
 import hanalyst.application.hanalystclub.Util.AnalysisFactory;
+import hanalyst.application.hanalystclub.lifecycle.viewmodels.PlayerViewModel;
 import hanalyst.application.hanalystclub.repository.PlayerRepository;
 
-public class Analysis extends Activity {
+public class Analysis extends AppCompatActivity {
 
     GridView gridAttack, gridDefence;
     String currentTime = "";
@@ -37,7 +46,7 @@ public class Analysis extends Activity {
     int minute = 0, second = 0;
     TextView timerTextView, pauseText;
     boolean pause = false;
-
+    PlayerViewModel playerViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +63,8 @@ public class Analysis extends Activity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
+
+        playerViewModel = new ViewModelProvider(this).get(PlayerViewModel.class);
         gridAttack = findViewById(R.id.grid_attack);
         gridDefence = findViewById(R.id.grid_defence);
         timerTextView = findViewById(R.id.timer);
@@ -79,21 +90,31 @@ public class Analysis extends Activity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 int prev = defence.get(position).getValue();
                 defence.get(position).setValue(prev + 1);
-                final Dialog dialog = new Dialog(Analysis.this);
+                final Dialog dialog = new Dialog(Analysis.this, R.style.FullHeightDialog);
+
                 dialog.setContentView(R.layout.player_list_who);
+
+                if (dialog.getWindow() != null){
+                    dialog.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                }
+
+                RecyclerView recyclerView = dialog.findViewById(R.id.players_list);
+
+                final PlayersAdapter playersAdapter = new PlayersAdapter();
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                recyclerView.setAdapter(playersAdapter);
+
+
+
+
+                recyclerView.setAdapter(playersAdapter);
+                playerViewModel.getAllPlayers().observe(Analysis.this, new Observer<List<Player>>() {
+                    @Override
+                    public void onChanged(List<Player> players) {
+                        playersAdapter.setPlayers(players);
+                    }
+                });
                 dialog.show();
-
-//                PlayerRepository playerRepository = new PlayerRepository(getApplicationContext());
-                final ArrayList<String> arrayList = new ArrayList<>();
-//                playerRepository.getPlayers().observe(null, new Observer<List<Player>>() {
-//                    @Override
-//                    public void onChanged(@Nullable List<Player> players) {
-//                        for (Player player : players) {
-//                            arrayList.add(player.getName());
-//                        }
-//                    }
-//                });
-
                 gridDefence.setAdapter(new DefenseAdapter(getApplicationContext(), defence));
             }
         });
@@ -147,6 +168,10 @@ public class Analysis extends Activity {
                 }
             });
         }
+    }
+
+    public void saveNotation() {
+
     }
 
 }
