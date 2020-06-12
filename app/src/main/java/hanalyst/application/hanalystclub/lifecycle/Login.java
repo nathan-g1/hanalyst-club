50df45093246235066e9dfd556436b8208d09686
Nathan Getachew <nathygech111@gmail.com>
2020-06-12 09:37:14 +0300

    Remove programmatic fullscreen setting

--- app/src/main/java/hanalyst/application/hanalystclub/lifecycle/Login.java
+++ app/src/main/java/hanalyst/application/hanalystclub/lifecycle/Login.java
@@ -43,17 +43,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         sharedPreferenceHAn = new SharedPreferenceHAn(Login.this);
         if (!sharedPreferenceHAn.getTeamId().equals("null")) {
