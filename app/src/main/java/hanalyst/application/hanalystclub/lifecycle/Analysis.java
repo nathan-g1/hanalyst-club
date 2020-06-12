9031c02f61cdfc815cbc0132a63b41579f5a0af6
Nathan Getachew <nathygech111@gmail.com>
2020-06-12 09:48:29 +0300

    Update Analysis
    - Get notation on attack

--- app/src/main/java/hanalyst/application/hanalystclub/lifecycle/Analysis.java
+++ app/src/main/java/hanalyst/application/hanalystclub/lifecycle/Analysis.java
@@ -22,7 +22,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import hanalyst.application.hanalystclub.Adapter.AttackAdapter;
 import hanalyst.application.hanalystclub.Adapter.DefenseAdapter;
-import hanalyst.application.hanalystclub.Adapter.PlayersAdapter;
+import hanalyst.application.hanalystclub.Adapter.PlayersAdapterWithOnClick;
 import hanalyst.application.hanalystclub.Entity.Attack;
 import hanalyst.application.hanalystclub.Entity.Defense;
 import hanalyst.application.hanalystclub.Entity.Player;
@@ -36,10 +36,13 @@ public class Analysis extends AppCompatActivity {
     String currentTime = "";
     Timer timerObj = new Timer();
     int minute = 0, second = 0;
-    TextView timerTextView, pauseText;
+    TextView timerTextView, pauseText, effAttack, effDefence;
     boolean pause = false;
     PlayerViewModel playerViewModel;
     int zone = 0;
+    double attEffectiveness = 0;
+    double defEffectiveness = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +55,9 @@ public class Analysis extends AppCompatActivity {
         gridDefence = findViewById(R.id.grid_defence);
         timerTextView = findViewById(R.id.timer);
         pauseText = findViewById(R.id.pause);
+        effAttack = findViewById(R.id.textView_attack_effectiveness);
+        effDefence = findViewById(R.id.textView_defence_effectiveness);
+
         // Attack
         final ArrayList<Attack> attacks = new AnalysisFactory().getAttackList();
         final AttackAdapter attackAdapter = new AttackAdapter(getApplicationContext(), attacks);
@@ -60,7 +66,79 @@ public class Analysis extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 int prev = attacks.get(position).getValue();
-                attacks.get(position).setValue(prev + 1);
+                final Dialog dialog = new Dialog(Analysis.this, R.style.FullHeightDialog);
+
+                dialog.setContentView(R.layout.player_list_who);
+
+                if (dialog.getWindow() != null) {
+                    dialog.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                }
+
+                RecyclerView recyclerView = dialog.findViewById(R.id.players_list);
+
+                final PlayersAdapterWithOnClick playersAdapterWithOnClick = new PlayersAdapterWithOnClick();
+                playersAdapterWithOnClick.setOnItemClickListener(new PlayersAdapterWithOnClick.ClickListener() {
+                    @Override
+                    public void onItemClick(int playerPosition, View v) {
+                        final Dialog dialog2 = new Dialog(Analysis.this, R.style.FullHeightDialog);
+                        dialog2.setTitle(getString(R.string.select_zone));
+                        dialog2.setContentView(R.layout.zone_where);
+                        Button button1 = dialog2.findViewById(R.id.btn1);
+                        Button button2 = dialog2.findViewById(R.id.btn2);
+                        Button button3 = dialog2.findViewById(R.id.btn3);
+                        Button button4 = dialog2.findViewById(R.id.btn4);
+                        Button button5 = dialog2.findViewById(R.id.btn5);
+                        Button button6 = dialog2.findViewById(R.id.btn6);
+                        Button button7 = dialog2.findViewById(R.id.btn7);
+                        Button button8 = dialog2.findViewById(R.id.btn8);
+                        Button button9 = dialog2.findViewById(R.id.btn9);
+                        Button button10 = dialog2.findViewById(R.id.btn10);
+                        Button button11 = dialog2.findViewById(R.id.btn11);
+                        Button button12 = dialog2.findViewById(R.id.btn12);
+                        ArrayList<Button> arrayList = new ArrayList<>();
+
+                        dialog2.show();
+                        arrayList.add(button1);
+                        arrayList.add(button2);
+                        arrayList.add(button3);
+                        arrayList.add(button4);
+                        arrayList.add(button5);
+                        arrayList.add(button6);
+                        arrayList.add(button7);
+                        arrayList.add(button8);
+                        arrayList.add(button9);
+                        arrayList.add(button10);
+                        arrayList.add(button11);
+                        arrayList.add(button12);
+
+                        for (Button button : arrayList) {
+                            button.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    zone = arrayList.indexOf(button) + 1;
+                                    saveNotation(attacks.get(position), zone, playerPosition, currentTime);
+                                    Toast.makeText(Analysis.this, zone + " is zone " + attacks.get(position).getDesc(), Toast.LENGTH_SHORT).show();
+                                    attacks.get(position).setValue(prev + 1);
+                                    gridAttack.setAdapter(new AttackAdapter(getApplicationContext(), attacks));
+                                    atEffectiveness(attacks, effDefence);
+                                    dialog.dismiss();
+                                    dialog2.dismiss();
+                                }
+                            });
+                        }
+                    }
+                });
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+
+                recyclerView.setAdapter(playersAdapterWithOnClick);
+                playerViewModel.getAllPlayers().observe(Analysis.this, new Observer<List<Player>>() {
+                    @Override
+                    public void onChanged(List<Player> players) {
+                        playersAdapterWithOnClick.setPlayers(players);
+                    }
+                });
+                dialog.show();
                 gridAttack.setAdapter(new AttackAdapter(getApplicationContext(), attacks));
             }
         });
