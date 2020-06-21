387acdc03500e55c1af2ec2df69cc62f9e674ab4
Nathan Getachew <nathygech111@gmail.com>
2020-06-21 13:14:49 +0300

    Update Room converters

--- app/src/main/java/hanalyst/application/hanalystclub/Database/RoomConverter.java
+++ app/src/main/java/hanalyst/application/hanalystclub/Database/RoomConverter.java
@@ -1,19 +1,18 @@
 package hanalyst.application.hanalystclub.Database;
 
 import androidx.room.TypeConverter;
-import androidx.room.TypeConverters;
 import hanalyst.application.hanalystclub.Entity.Temperature;
 
 public class RoomConverter {
-    @TypeConverter
-    public String getTemperature(Temperature temperature) {
-        return temperature.getValue() + " " + temperature.getScaleType();
-    }
-
-    @TypeConverter
-    public Temperature setTemperature(String temp) {
-        return new Temperature(temp.split(" ")[0], Double.parseDouble(temp.split(" ")[1]));
-    }
+//    @TypeConverter
+//    public String getTemperature(String temperature) {
+//        return temperature.getValue() + " " + temperature.getScaleType();
+//    }
+//
+//    @TypeConverter
+//    public String setTemperature(String temp) {
+//        return new Temperature(temp.split(" ")[0], Double.parseDouble(temp.split(" ")[1]));
+//    }
 
     @TypeConverter
     public String getPlayingTeams(String[] teams) {
