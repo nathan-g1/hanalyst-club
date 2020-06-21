06fd918a36470ec80f3b8a220d2245088f6b5727
Nathan Getachew <nathygech111@gmail.com>
2020-06-21 13:13:43 +0300

    Display list of games in recyclerview

--- app/src/main/java/hanalyst/application/hanalystclub/lifecycle/fragments/FragmentRecent.java
+++ app/src/main/java/hanalyst/application/hanalystclub/lifecycle/fragments/FragmentRecent.java
@@ -8,8 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import hanalyst.application.hanalystclub.Adapter.RecentGamesAdapter;
+import hanalyst.application.hanalystclub.Entity.Game;
 import hanalyst.application.hanalystclub.R;
 import hanalyst.application.hanalystclub.lifecycle.HomeActivity;
+import hanalyst.application.hanalystclub.lifecycle.viewmodels.GameViewModel;
+import hanalyst.application.hanalystclub.lifecycle.viewmodels.NotationViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,13 +29,28 @@ public class FragmentRecent extends Fragment {
     public FragmentRecent() {
         // Required empty public constructor
     }
-
+    private GameViewModel gameViewModel;
+    private List<Game> gameList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ((HomeActivity) getActivity()).setTitle("Recent");
-        return inflater.inflate(R.layout.fragment_recent, container, false);
+        View view = inflater.inflate(R.layout.fragment_recent, container, false);
+
+        gameViewModel = new ViewModelProvider(getActivity()).get(GameViewModel.class);
+
+        RecyclerView recyclerView  = view.findViewById(R.id.recent_games_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        RecentGamesAdapter recentGamesAdapter = new RecentGamesAdapter();
+        gameViewModel.getAllGames().observe(getActivity(), recentGamesAdapter::setGames);
+        gameList = recentGamesAdapter.getGivenGames();
+        recentGamesAdapter.setOnItemClickListener((position, v) -> {
+
+        });
+        recyclerView.setAdapter(recentGamesAdapter);
+        return view;
     }
 }
