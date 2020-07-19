e7d501198cae9c23c73a615f01b0fe797d00c60e
Nathan Getachew <nathygech111@gmail.com>
2020-07-20 00:04:47 +0300

    :construction: WIP on recent game & recent game details

---
+++ app/src/main/java/hanalyst/application/hanalystclub/Adapter/helper/ViewHolderContentSupplier.java
@@ -0,0 +1,13 @@
+package hanalyst.application.hanalystclub.Adapter.helper;
+
+import hanalyst.application.hanalystclub.lifecycle.viewmodels.GameViewModel;
+
+public class ViewHolderContentSupplier {
+    private GameViewModel gameViewModel;
+
+    public ViewHolderContentSupplier(GameViewModel gameViewModel) {
+        this.gameViewModel = gameViewModel;
+    }
+
+
+}
