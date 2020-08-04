fa19f251c8f313a71ed9c261f48f38c4e9369e50
Nathan Getachew <nathygech111@gmail.com>
2020-08-04 22:57:16 +0300

    :sparkles: Language Change
    - Create Language enum
    - Language chooser fragment
    - new string resources
    - Create Language Options Adapter
    - :fire: Update fragment passowrd(code formatting)

---
+++ app/src/main/java/hanalyst/application/hanalystclub/Entity/Language.java
@@ -0,0 +1,28 @@
+package hanalyst.application.hanalystclub.Entity;
+
+public enum Language {
+    ENGLISH("English", "en"),
+    AMHARIC("Amharic", "am"),
+    OROMIFFA("Oromiffa", "om"),
+    SOMALI("Somali", "so"),
+    TIGRIGNA("Tigrigna", "ti");
+
+    private final String type;
+    private final String shortCode;
+
+    Language(String type, String shortCode) {
+        this.type = type;
+        this.shortCode = shortCode;
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+
+    public String getLanguageType() {
+        return shortCode;
+    }
+}
+
+
